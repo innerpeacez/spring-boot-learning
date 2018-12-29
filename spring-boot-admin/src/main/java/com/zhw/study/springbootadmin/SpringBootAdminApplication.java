e4bc7af7372701aa51f38510f29638e9d09736b8
innerpeacez <zhaihw0417@163.com>
2018-12-29 16:55:44 +0800

    spring boot admin学习

---
+++ spring-boot-admin/src/main/java/com/zhw/study/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,22 @@
+package com.zhw.study.springbootadmin;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+    @Bean
+    public SecuritySecureConfig securitySecureConfig() {
+        return new SecuritySecureConfig(new AdminServerProperties());
+    }
+}
+
