833cf80536836dc92ddaf23ec5bcc5d4c7070512
innerpeacez <zhaihw0417@163.com>
2019-01-02 17:12:08 +0800

    spring boot admin学习

--- spring-boot-admin/src/main/java/com/zhw/study/springbootadmin/SecuritySecureConfig.java
+++ spring-boot-admin/src/main/java/com/zhw/study/springbootadmin/SecuritySecureConfig.java
@@ -1,7 +1,6 @@
 package com.zhw.study.springbootadmin;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
