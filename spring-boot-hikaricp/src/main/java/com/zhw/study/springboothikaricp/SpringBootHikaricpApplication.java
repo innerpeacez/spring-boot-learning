2f9bba8fe183153bdc2377e90693e32888a4d9a6
innerpeacez <zhaihw0417@163.com>
2019-01-03 18:20:55 +0800

    HikariCP链接池

---
+++ spring-boot-hikaricp/src/main/java/com/zhw/study/springboothikaricp/SpringBootHikaricpApplication.java
@@ -0,0 +1,14 @@
+package com.zhw.study.springboothikaricp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHikaricpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHikaricpApplication.class, args);
+    }
+
+}
+
