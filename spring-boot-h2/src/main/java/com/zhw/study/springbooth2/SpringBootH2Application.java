0c57255a0e4c7b5e7998e3af545cfd5f6850a9e9
innerpeacez <zhaihw0417@163.com>
2019-01-18 14:26:15 +0800

    spring boot h2

---
+++ spring-boot-h2/src/main/java/com/zhw/study/springbooth2/SpringBootH2Application.java
@@ -0,0 +1,14 @@
+package com.zhw.study.springbooth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootH2Application.class, args);
+    }
+
+}
+
