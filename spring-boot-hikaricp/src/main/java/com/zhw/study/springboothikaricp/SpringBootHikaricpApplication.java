530adcc343dc44e3cb9b87d16baff9b14f11ae49
innerpeacez <zhaihw0417@163.com>
2019-02-14 19:44:18 +0800

    datasource schema使用

--- spring-boot-hikaricp/src/main/java/com/zhw/study/springboothikaricp/SpringBootHikaricpApplication.java
+++ spring-boot-hikaricp/src/main/java/com/zhw/study/springboothikaricp/SpringBootHikaricpApplication.java
@@ -1,14 +1,15 @@
 package com.zhw.study.springboothikaricp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootHikaricpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHikaricpApplication.class, args);
     }
-
 }
 
---
+++ spring-boot-hikaricp/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO foo (ID, BAR) VALUES (1, 'aaa');
+INSERT INTO foo (ID, BAR) VALUES (2, 'bbb');
