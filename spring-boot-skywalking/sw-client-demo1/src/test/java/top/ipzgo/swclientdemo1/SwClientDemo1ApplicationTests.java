f5f743552f179a54f3645313b8f776faaa6ad21f
innerpeacez <innerpeace.zhai@gmail.com>
2020-01-03 13:17:11 +0800

    rm .idea

---
+++ spring-boot-skywalking/sw-client-demo1/src/test/java/top/ipzgo/swclientdemo1/SwClientDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ipzgo.swclientdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwClientDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
